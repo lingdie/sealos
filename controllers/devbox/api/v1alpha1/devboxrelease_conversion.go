cf000fd4cc8917d81106d25d9380e7be4865dd0a
yy <lingdie.yy@outlook.com>
2025-10-17 11:48:31 +0800

    fix devbox release owner reference conversion

--- controllers/devbox/api/v1alpha1/devboxrelease_conversion.go
+++ controllers/devbox/api/v1alpha1/devboxrelease_conversion.go
@@ -1,14 +1,31 @@
 package v1alpha1
 
 import (
-	devboxv1alpha2 "github.com/labring/sealos/controllers/devbox/api/v1alpha2"
-
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
+
+	devboxv1alpha2 "github.com/labring/sealos/controllers/devbox/api/v1alpha2"
 )
 
 // ConvertTo converts this DevBoxRelease (v1alpha1) to the Hub version (v1alpha2).
 func (src *DevBoxRelease) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*devboxv1alpha2.DevBoxRelease)
+
+	// Copy ObjectMeta to preserve name, namespace, labels, etc.
+	dst.ObjectMeta = src.ObjectMeta
+
+	// Set TypeMeta for the target version: v1alpha2
+	dst.TypeMeta = metav1.TypeMeta{
+		APIVersion: "devbox.sealos.io/v1alpha2",
+		Kind:       "DevBoxRelease",
+	}
+	dst.OwnerReferences = make([]metav1.OwnerReference, len(src.OwnerReferences))
+	copy(dst.OwnerReferences, src.OwnerReferences)
+	for i := range dst.OwnerReferences {
+		dst.OwnerReferences[i].APIVersion = "devbox.sealos.io/v1alpha2"
+	}
+
+	// Transform DevBoxRelease from v1alpha1 to v1alpha2
 	dst.Spec.DevboxName = src.Spec.DevboxName
 	dst.Spec.Version = src.Spec.NewTag
 	dst.Spec.Notes = src.Spec.Notes
@@ -21,6 +38,22 @@ func (src *DevBoxRelease) ConvertTo(dstRaw conversion.Hub) error {
 
 func (dst *DevBoxRelease) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*devboxv1alpha2.DevBoxRelease)
+
+	// Copy ObjectMeta to preserve name, namespace, labels, etc.
+	dst.ObjectMeta = src.ObjectMeta
+
+	// Set TypeMeta for the target version: v1alpha1
+	dst.TypeMeta = metav1.TypeMeta{
+		APIVersion: "devbox.sealos.io/v1alpha1",
+		Kind:       "DevBoxRelease",
+	}
+	dst.OwnerReferences = make([]metav1.OwnerReference, len(src.OwnerReferences))
+	copy(dst.OwnerReferences, src.OwnerReferences)
+	for i := range dst.OwnerReferences {
+		dst.OwnerReferences[i].APIVersion = "devbox.sealos.io/v1alpha1"
+	}
+
+	// Transform DevBoxRelease from v1alpha2 to v1alpha1
 	dst.Spec.DevboxName = src.Spec.DevboxName
 	dst.Spec.NewTag = src.Spec.Version
 	dst.Spec.Notes = src.Spec.Notes
