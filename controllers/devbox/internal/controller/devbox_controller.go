e5a47f3461d1e205fcf14931f177888f70e636c2
Zllinc <2965202581@qq.com>
2025-11-14 17:07:21 +0800

    fix: commit records nil pointer error

--- controllers/devbox/internal/controller/devbox_controller.go
+++ controllers/devbox/internal/controller/devbox_controller.go
@@ -168,6 +168,11 @@ func (r *DevboxReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		return ctrl.Result{}, err
 	}
 
+	// check content id and commit record is exist
+	if devbox.Status.CommitRecords == nil || devbox.Status.CommitRecords[devbox.Status.ContentID] == nil {
+		return ctrl.Result{Requeue: true}, nil
+	}
+
 	// todo: implement the schedule logic to replace the current logic
 	// if devbox state is running, schedule devbox to node, update devbox status and create a new commit record
 	// and filter out the devbox that are not in the current node
